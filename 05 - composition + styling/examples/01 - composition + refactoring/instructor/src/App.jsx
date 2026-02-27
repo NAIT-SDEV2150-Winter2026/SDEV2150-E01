c53ad0489e6e8e0063586b44e03cf824ceebe31b
oaao <oliver+github@abolish.golf>
2026-02-26 17:36:42 -0700

    refactor [05 - composition]: ex01 - 'for fun', change Results to take data as a prop

--- 05 - composition + styling/examples/01 - composition + refactoring/instructor/src/App.jsx
+++ 05 - composition + styling/examples/01 - composition + refactoring/instructor/src/App.jsx
@@ -2,6 +2,9 @@ import Header from './components/Header';
 import Filters from './components/Filters';
 import Results from './components/Results';
 import Details from './components/Details';
+
+import { resources } from './data/resources';
+
 import './App.css';
 
 function App() {
@@ -13,7 +16,7 @@ function App() {
           <Filters />
         </div>
         <div className="w-full">
-          <Results />
+          <Results resources={resources} />
         </div>
         <div className="w-full">
           <Details />
