9308692673b4967e32aeadf722939e635671d91c
Oliver Antoniu <oliver+github@abolish.golf>
2026-03-09 17:30:59 -0600

    add [07 - hooks]: ex01 - set up skeleton for custom hook & session storage

---
+++ 07 - hooks + lifecycle/examples/01 - hooks/instructor/src/hooks/useSelectedResource.js
@@ -0,0 +1,14 @@
+
+// This will be the name of the 'key' that we'll write session data to for which row is selected.
+// Session storage operates like a key-value store (e.g. like a Python dictionary).
+const STORAGE_KEY = 'selectedResource'
+
+export function useSelectedResource() {
+	// We'll be using this to set a stateful value and attach it to session storage.
+
+
+
+	export function updateSelectedResource(resource) {
+		// We'll be using this to write a resource to the session storage.
+	}
+}
