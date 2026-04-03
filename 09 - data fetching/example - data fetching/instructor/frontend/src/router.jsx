2dadda4a6b5ffdbd624b27c6708c97a225bd82fb
oaao <oliver+github@abolish.golf>
2026-04-02 18:25:06 -0600

    add [09 - data fetching]: ex - make a new router file, add in API functions

---
+++ 09 - data fetching/example - data fetching/instructor/frontend/src/router.jsx
@@ -0,0 +1,25 @@
+// 1. API config / functions --------------------------------------------------
+
+const API_BASE_URL = 'http://localhost:3000';
+
+
+async function fetchResources() {
+  const res = await fetch(`${API_BASE_URL}/resources`);
+
+  if (!res.ok) {
+    throw new Error(`Could not load resources: ${res.status}`);
+  }
+
+  return res.json();
+}
+
+
+async function fetchResourceById(resourceId) {
+  const res = await fetch(`${API_BASE_URL}/resources/${resourceId}`);
+
+  if (!res.ok) {
+    throw new Error(`Could not load resource: ${res.status}`);
+  }
+
+  return res.json();
+}
